f107466cb49148005228f3962cce7d41d010f302
abhisekm <abhisekmajumder07@gmail.com>
2019-09-19 03:07:22 +0530

    reusable component

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>;
-}
+  return (
+    <View>
+      <ImageDetail
+        title="forest"
+        imageSource={require('../../assets/forest.jpg')}
+        score={5} />
+      <ImageDetail
+        title="beach"
+        imageSource={require('../../assets/beach.jpg')}
+        score={10} />
+      <ImageDetail
+        title="mountain"
+        imageSource={require('../../assets/mountain.jpg')}
+        score={4} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({});
 
