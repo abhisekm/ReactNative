dd591f3389bad3bdd9e6c47c63c54d4be729008b
abhisekm <abhisekmajumder07@gmail.com>
2019-09-19 02:43:24 +0530

    Touchable Opacity

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   return (
     <View>
       <Text style={styles.text}>Hello World!</Text>
       <Button
-        onPress={() => {console.log('Button Pressed')}}
-        title="Go to Components Demo " />
+        onPress={() => { navigation.navigate('Components') }}
+        title="Go to Components Demo" />
+         <Button
+        onPress={() => { navigation.navigate('List') }}
+        title="Go to List Demo" />
+      {/* <TouchableOpacity onPress={() => { props.navigation.navigate('List') }}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
     </View>
   );
 };
