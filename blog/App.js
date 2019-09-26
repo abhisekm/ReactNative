d379d05bf82b7219d13d8eaf5b82b3be662aae9b
abhisekm <abhisekmajumder07@gmail.com>
2019-09-26 14:50:56 +0530

    Added show screen

--- blog/App.js
+++ blog/App.js
@@ -3,10 +3,12 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screen/IndexScreen';
 import { Provider as BlogProvider } from './src/context/BlogContext';
+import ShowScreen from './src/screen/ShowScreen';
 
 const navigator = createStackNavigator(
   {
-    Index: IndexScreen
+    Index: IndexScreen,
+    Show: ShowScreen
   },
   {
     initialRouteName: 'Index',
