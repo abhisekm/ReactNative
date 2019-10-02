136f74a81700c8f8fe3eb81b32fc445014116f30
abhisekm <abhisekmajumder07@gmail.com>
2019-10-02 18:31:00 +0530

    added auth

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -7,38 +7,47 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
-    case 'signup':
+    case 'signin':
       return { errorMessage: '', token: action.payload };
+    case 'clear_error_message':
+      return { ...state, errorMessage: '' };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
-    try {
-      // make api call to sign up
-      const response = await trackerApi.post('/signup', { email, password });
-      await AsyncStorage.setItem('token', response.data.token);
-
-      // if we sign up, modify our state and say we are authenticated
-      dispatch({ type: 'signup', payload: response.data.token })
-      navigate('TrackList');
-    } catch (err) {
-      // if signup failed, how an error msg
-      dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
-    }
-  };
+const clearErrorMessage = dispatch => () => {
+  dispatch({ type: 'clear_error_message' });
 };
 
-const signin = (dispatch) => {
-  return ({ email, password }) => {
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    // make api call to sign up
+    const response = await trackerApi.post('/signup', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+
+    // if we sign up, modify our state and say we are authenticated
+    dispatch({ type: 'signin', payload: response.data.token })
+    navigate('TrackList');
+  } catch (err) {
+    // if signup failed, how an error msg
+    dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
+  }
+};
+
+const signin = (dispatch) => async ({ email, password }) => {
+  try {
     //Sign in via api
+    const response = await trackerApi.post('/signin', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
 
     //Handle success
-
+    dispatch({ type: 'signin', payload: response.data.token })
+    navigate('TrackList');
+  } catch (err) {
     //handle failure
-  };
+    dispatch({ type: 'add_error', payload: 'Something went wrong with sign in' })
+  }
 };
 
 const signout = (dispatch) => {
@@ -49,6 +58,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signup, signout },
+  { signin, signup, signout, clearErrorMessage },
   { token: null, errorMessage: '' }
 );
--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = ({ navigation }) => {
-  return (
-    <>
-      <Text>SigninScreen</Text>
-      <Button
-        title="Go to home"
-        onPress={() => navigation.navigate('mainFlow')}
-      />
-    </>
-  );
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
 
+  return (
+    <View style={styles.container}>
+      <NavigationEvents
+        onWillBlur={clearErrorMessage}
+      />
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign In"
+        onSubmit={signin}
+      />
+      <NavLink
+        routeName="Signup"
+        text="Dont have an account? Sign up instead"
+      />
+    </View>
+  );
+};
+
+SigninScreen.navigationOptions = () => {
+  return {
+    header: null
+  };
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200
+  }
 });
+
 
 export default SigninScreen;
